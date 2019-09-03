a22ad0259151334657821244990f8c748f99e90d
Hannah Hudson <hahudson@cisco.com>
2019-09-02 19:00:45 -0600

    additional data

--- src/pages/people/index.tsx
+++ src/pages/people/index.tsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
-interface IPerson {
+interface IPersonDisplay {
   name: string;
   homeworld: string;
   homeworld_name: string;
   population: string;
+  species: any;
+  species_name: string;
+  language: string;
+}
+
+interface IPersonRequest {
+  homeworld: RequestInfo;
+  homeworld_name: string;
+  population: string;
+  species: RequestInfo;
+  species_name: string;
+  language: string;
 }
 
 export const People: React.FC = () => {
@@ -12,43 +24,52 @@ export const People: React.FC = () => {
 
   useEffect(() => {
     fetch(`https://swapi.co/api/people/`)
-      .then(response => response.json())    
-      .then(response => (response.results).forEach((person: IPerson) => {
-        fetch(person.homeworld)
-            .then(r => r.json())
-            .then(r => setData([
-              ...response.results,
-              person.homeworld_name = r.name,
-              person.population = r.population
-            ]))
-        }))
-      .catch(error => console.log(error))
-
+      .then(response => response.json())  
+      .then(response => {
+        setData(response.results);
+        fetchAdditional(response.results);
+      })
+      .catch((error) => console.log(error))  
   }, []);
 
+  const fetchAdditional = (people: any) => {
+    people.forEach((person: IPersonRequest) => {
+      fetch(person.homeworld)
+      .then(response => response.json())
+      .then(response => {
+        person.homeworld_name = response.name;
+        person.population = response.population;
+        setData([...people])
+      })
+      fetch(person.species)
+        .then(response => response.json())
+        .then(response => {
+          person.species_name = response.name;
+          person.language = response.language;
+          setData([...people])
+      })
+    }) 
+  }
+
   return (
-    
-    <>
+
     <div className="d-flex flex-wrap justify-content-around">
      
-        {data ? (
-          
-          data.map((person: IPerson, index: number) =>
+        {data ? (     
+          data.map((person: IPersonDisplay, index: number) =>
           
             <div className="page-card" key={ index }>
               <h2 className="page-card-title p-2">{ person.name }</h2>
               <div className="p-2">
               <h3 className="page-card-text">
-                Homeworld:{ person.homeworld_name }</h3>
-                <h3 className="page-card-text">Species: </h3>
-                <h3 className="page-card-text">Language: </h3>
+                Homeworld: { person.homeworld_name }</h3>
+                <h3 className="page-card-text">Species: { person.species_name }</h3>
+                <h3 className="page-card-text">Language: { person.language }</h3>
                 <h3 className="page-card-text">Population: { person.population }</h3>
               </div>
             </div>
           )
-        ) : null}
-    </div>
-       
-    </>
+        ) : <h1>loading...</h1>}
+    </div>       
   )
 }
